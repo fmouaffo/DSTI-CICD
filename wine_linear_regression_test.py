615027467903b07e4f6f1bf6b5654e29a4bbb463
fmouaffo <39360168+fmouaffo@users.noreply.github.com>
2021-08-04 12:16:56 +0200

    Update wine_linear_regression_test.py

--- wine_linear_regression_test.py
+++ wine_linear_regression_test.py
@@ -75,7 +75,7 @@ def train(alpha=0.3, l1_ratio=0.5):
         outfile.write("  MAE: %s" % mae)
         outfile.write("  R2: %s" % r2)
 
-train()
+train(alpha = 0.4, l1_ratuib = 0.6)
 
 # COMMAND ----------
 
